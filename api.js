371dd7e4a29e185af4594d90fdd4265c457fde73
Zee Celest <zeecelest@gmail.com>
2019-10-01 11:32:01 -0400

    adding function tabs in api.js

--- api.js
+++ api.js
@@ -4,6 +4,16 @@
 
 const url = 'https://thinkful-list-api.herokuapp.com';
 
+//user will be able to add a new bookmark
+//bookmark should include title, url, description, rating
+const addBookmark() = function() {
+
+}
+
+const deleteBookmark() = function() {
+
+}
+
 export default {
-    
+
 }
