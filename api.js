fcb036dfba85a9e89123691453c656939053029f
Zee Celest <zeecelest@gmail.com>
2019-10-01 14:17:44 -0400

    edit wireframe, add function tags, and pseudocode

--- api.js
+++ api.js
@@ -6,13 +6,46 @@ const url = 'https://thinkful-list-api.herokuapp.com';
 
 //user will be able to add a new bookmark
 //bookmark should include title, url, description, rating
-const addBookmark() = function() {
+
+//functions working with store in store.js, api in api.js and so on
+
+//user can see a list of their bookmarks when they first open the app
+//list defaults to condensed view showing only title and rating
+const bookmarkList() = function() {
+    
+
+};
+
+//click a bookmark to display the detailed view
+//expands to also display description and "Visit Site" link
+const detailedBookmarks = function () {
 
 }
 
+
+const addBookmark() = function() {
+    bookmarks {
+        title: "", required;
+        url: "", required;
+        description: "",
+        rating: 1 - 5
+
+    }
+
+}
+
+//use <select> element
+//user can select from dropdown a minimum rating to filter the list
+//by bookmarks rated at or above the chosen selection
+const dropdown() = function() {
+
+}
+
+//user can remove bookmark from bookmark list
 const deleteBookmark() = function() {
 
 }
+
 
 export default {
 
---
+++ index.js
@@ -0,0 +1,1 @@
+//file that initialize the app
